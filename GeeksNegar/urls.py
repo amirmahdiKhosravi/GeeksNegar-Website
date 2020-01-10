bf150c5e131dab639b89708eeff3e3ed1c2fd331
root <root@localhost.localdomain>
2020-01-11 00:01:55 +0330

    blog's url added to site urlpaterns and some basic code in blog/urls.py

--- GeeksNegar/urls.py
+++ GeeksNegar/urls.py
@@ -20,6 +20,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^blog/', include('blog.urls'))
 ]
 
 # SET STATIC URLS :
---
+++ blog/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls import url
+from . import views             #importing views of this app to joind them to URLS
+
+app_name= 'blog'                #to find app's url better and faster
+urlpatterns = [
+    url(r'^$/', )
+]
