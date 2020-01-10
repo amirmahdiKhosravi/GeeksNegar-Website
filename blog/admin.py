03338ae9880a45f7ed28478c6a91b585cf94235c
nima <nima.different76@gmail.com>
2020-01-10 14:58:12 +0330

    basic admin views added, some changes in models

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from .models import Image, MemberRole, Post, TeamMember, Video
 
-# Register your models here.
+@admin.register(Image)
+class ImageAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Video)
+class VideoAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(MemberRole)
+class MemberRoleAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(TeamMember)
+class TeamMemberAdmin(admin.ModelAdmin):
+    pass
+    
+class TeamMemberInline(admin.TabularInline):
+    model = TeamMember
+    extra = 2
+    
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_filter = ('title', 'text','pub_date')
+    list_display = ('id','title', 'published', 'get_members')
+    def get_members(self, obj):
+        return " | ".join([p.user.get_username() for p in obj.member.all()])
+    get_members.short_description = 'member(s)'
--- blog/models.py
+++ blog/models.py
@@ -5,11 +5,12 @@ from django.contrib.auth.models import User
 class Post(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text="Unique ID for this particular post")
     title = models.CharField(max_length=255)
-    member = models.ManyToManyField('TeamMember')
-    text = models.TextField(max_length=1000)
+    text = models.TextField(max_length=255,blank=True)
     published = models.BooleanField()
     pub_date = models.DateField()
-    video = models.ForeignKey('Video',on_delete=models.CASCADE, null=True) 
+    video = models.ForeignKey('Video',on_delete=models.CASCADE, null=True , blank = True) 
+    image = models.ForeignKey('Image',on_delete=models.CASCADE, null=True , blank = True)
+    member = models.ManyToManyField('TeamMember')
 
     def __str__(self):
         return self.title
@@ -31,7 +32,7 @@ class Image(models.Model):
 class TeamMember(models.Model):
     user = models.ForeignKey(User,on_delete=models.CASCADE)
     role = models.ManyToManyField('MemberRole')
-
+    
     def __str__(self):
         roles_str = ", ".join(str(r) for r in self.role.all())
         return str(self.user.get_username())+' | '+roles_str 
