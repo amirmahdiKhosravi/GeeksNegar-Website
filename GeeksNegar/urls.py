8643bc7fb454992f7783d6968bf668a0eae0e407
nima <nima.different76@gmail.com>
2020-01-11 23:19:48 +0330

    simple index.html created, media root changed, view.py changed to show index.html

--- GeeksNegar/urls.py
+++ GeeksNegar/urls.py
@@ -17,11 +17,16 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
+
     url(r'^admin/', admin.site.urls),
-    url(r'^blog/', include('blog.urls'))
+    url(r'^blog/', include('blog.urls')),
+
 ]
 
 # SET STATIC URLS :
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from . import models
+from GeeksNegar import settings
 
 def index(request):
-    return HttpResponse("hello world!")
+    post_list =  models.Post.objects.order_by('-pub_date')
+    context={'post_list' : post_list
+    }
+    return render(request, 'blog/index.html',context=context)
