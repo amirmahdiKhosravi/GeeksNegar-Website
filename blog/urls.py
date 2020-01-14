e919f2af685ed34ddd20f819a461b4c85e79512c
root <root@localhost.localdomain>
2020-01-14 18:23:10 +0330

    Function_base_view changed into class_base_view

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
-from . import views             #importing views of this app to joind them to URLS
+from . import views                                  #importing views of this app to joind them to URLS
 
-app_name= 'blog'                #to find app's url better and faster
+app_name= 'blog'                                     #to find app's url better and faster
+
 urlpatterns = [
-    url(r'^$', views.index,name='index' )
+    url(r'^$', views.Index.as_view(),name='index' ) #we used as_view() function to reach Index class of our view
 ]
