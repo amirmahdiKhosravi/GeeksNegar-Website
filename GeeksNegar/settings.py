85dfa4da06d3867e6abbaad4133100bbdf098992
sarakvn <sarakvn@gmail.com>
2020-01-18 00:10:42 +0330

    user name and authentication added to header template

--- GeeksNegar/settings.py
+++ GeeksNegar/settings.py
@@ -121,3 +121,5 @@ USE_TZ = True
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 STATIC_URL = '/static/'
+EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
+EMAIL_FILE_PATH = os.path.join(BASE_DIR, "sent_emails")
