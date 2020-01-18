1ceb3040d58d56455bcf3ec268f4579f7d8aad45
sarakvn <sarakvn@gmail.com>
2020-01-19 00:56:36 +0330

    profile page added

--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views import generic
+from blog import models
 
 
 class SignUp(generic.CreateView):
@@ -10,6 +11,15 @@ class SignUp(generic.CreateView):
     template_name = 'accounts/signup.html'
 
 def ProfileView(request):
+    post_list =  models.Post.objects.all()
+    user_posts=[]
+    for post in post_list:
+        print(post.member.all())
+        for member in post.member.all():
+            if request.user==member.user:
+                print(post)
+                user_posts.append(post)
     context={
+        'post_list':user_posts,
     }
     return render(request, 'accounts/profile.html',context=context)
