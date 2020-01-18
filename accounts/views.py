e7eeabc390900fb5698334cafc65e43465d08bf3
sarakvn <sarakvn@gmail.com>
2020-01-18 14:44:37 +0330

    signup redirects to profile

--- accounts/views.py
+++ accounts/views.py
@@ -6,7 +6,7 @@ from django.views import generic
 
 class SignUp(generic.CreateView):
     form_class = UserCreationForm
-    success_url = reverse_lazy('login')
+    success_url = reverse_lazy('accounts:profile')
     template_name = 'accounts/signup.html'
 
 def ProfileView(request):
