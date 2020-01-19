1e733267affed11d40b8e6703983d83d38caf237
root <root@localhost.localdomain>
2020-01-19 15:14:42 +0330

    add,  delete and update post in profile.

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,9 +2,12 @@ from django.conf.urls import url
 
 from . import views
 
-app_name= 'accounts' 
+app_name= 'accounts'
 urlpatterns = [
     url(r'^signup/$', views.SignUp.as_view(), name='signup'),
     url(r'^profile/$', views.ProfileView, name='profile'),
     url(r'^profile/(?P<username>[a-zA-Z0-9]+)$', views.ProfileViewOther, name='profile-other'),
+    url(r'^profile/post/create/$', views.ProfilePostCreat, name='PostCreat'),
+    url(r'^profile/post/(?p<pk>\d+)/update/$', views.ProfilePostUpdate, name='PostUpdate'),
+    url(r'^profile/post/(?p<pk>\d+)/delete/$', views.ProfilePostDelete, name='PostDelete ')
 ]
