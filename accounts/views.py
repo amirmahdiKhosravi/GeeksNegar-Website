0607f7fd39b42638ae563205ff39fc77033289d0
nimaseyfi <nima.different76@gmail.com>
2020-01-19 15:54:07 +0330

    others profile added

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views import generic
+from django.contrib.auth.decorators import login_required
 from blog import models
 
 
@@ -10,6 +11,7 @@ class SignUp(generic.CreateView):
     success_url = reverse_lazy('accounts:profile')
     template_name = 'accounts/signup.html'
 
+@login_required
 def ProfileView(request):
     post_list =  models.Post.objects.all()
     user_posts=[]
@@ -23,3 +25,19 @@ def ProfileView(request):
         'post_list':user_posts,
     }
     return render(request, 'accounts/profile.html',context=context)
+
+def ProfileViewOther(request,username):
+    user = get_object_or_404(models.User, username=username)
+    post_list =  models.Post.objects.all()
+    user_posts=[]
+    for post in post_list:
+        print(post.member.all())
+        for member in post.member.all():
+            if user==member.user:
+                print(post)
+                user_posts.append(post)
+    context={
+        'user_other':user,
+        'post_list':user_posts,
+    }
+    return render(request, 'accounts/profile_other.html',context=context)
